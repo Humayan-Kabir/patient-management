cb667fcd93c53fb75a811adf3d13abae38f741c6
M. Humayan Kabir <humayan7711@gmail.com>
2025-07-09 14:48:14 +0600

    update billing service as grpc server

---
+++ billing-service/src/test/java/com/dev/healthcare/billing_service/BillingServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package com.dev.healthcare.billing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
